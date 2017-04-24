3d0c3279824a8cd1a931fdc2140a96503c093af0
YaoKeQi <yaokeqi1996@outlook.com>
2017-04-24 11:51:05 +0800

    gui

---
+++ src/top/LoginGui/RegisterMoniterInLG.java
@@ -0,0 +1,16 @@
+package top.LoginGui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by YaoKeQi on 2017/4/23.
+ * commit on 4/24
+ */
+public class RegisterMoniterInLG implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        new RegisterGui();
+    }
+}
---
+++ src/top/LoginGui/runGui.java
@@ -0,0 +1,11 @@
+package top.LoginGui;
+
+/**
+ * Created by YaoKeQi on 2017/4/23.
+ * commit on 4/24
+ */
+public class runGui {
+    public static void main (String[] args) {
+        new LoginGui();
+    }
+}
