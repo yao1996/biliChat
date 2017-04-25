194c19daca70b604d9441bfac4c472770daf7c32
YaoKeQi <yaokeqi1996@outlook.com>
2017-04-25 21:56:14 +0800

    1

---
+++ src/top/GUI/LoginGui/runGui.java
@@ -0,0 +1,11 @@
+package top.GUI.LoginGui;
+
+/**
+ * Created by YaoKeQi on 2017/4/23.
+ * commit on 4/24
+ */
+public class runGui {
+    public static void main (String[] args) {
+        new LoginGui();
+    }
+}
